9966880b318ebe2d67aef45ae55281e9d138568a
Natalia403 <natashka-5800@mail.ru>
2019-12-04 13:29:26 +0300

    learn-2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const a = 5;
 
-console.log('a', a);
+const b = [1, 2, 3, 4, 5, 6];
+
+const pow2 = arg => arg ** 2; /* функция*/
+
+function pow22(arg) {
+  return arg ** 2;
+}
+
+const aaa = 3;
+const bbb = "3";
+
+const c = b.map(pow22);
+
+const d = b.filter(el => el == "3");
+
+const str = `c[0]: ${c[0]}, c[5]: ${c[5]},`;
+
+console.log(!!1);
